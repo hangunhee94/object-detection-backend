84884a981d97ea89b31660265c251e08095fd8f9
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-05-20 20:58:23 +0900

    파일 데이터 응답 api 추가

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from audioop import findfactor
+from datetime import datetime
 from io import BytesIO
+import json
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from PIL import Image
@@ -19,35 +22,70 @@ def login_page():
     return render_template('index.html')
 
 
-@app.route('/posts', methods=['POST'])
-
+@app.route('/post', methods=['POST'])
 def post_file():
 # if user is not None:
     files = request.files.to_dict() # ImmutableMultiDict을 객체로 변환
-    file_list = []
     for file in files.values():
-        extension = file.filename.split('.')[-1]
-        format = 'JPEG' if extension.lower() == 'jpg' else extension.upper()
-        img = Image.open(file)
-        wpercent = (614/ float(img.size[0]))
-        h_size = int((float(img.size[1]) * float(wpercent)))
-        img_resize = img.resize((614, h_size))
-        buffered = BytesIO()
-        img_resize.save(buffered, format)
-        image_base64 = base64.b64encode(buffered.getvalue())
-        file_list.append(image_base64)
-    # user_id = user.get('id')
-    # user_nick_name = user.get('nick_name')
-    # content = request.form['content']
-    doc = {
-        # 'user_id': user_id,
-        # 'user_nick_name': user_nick_name,
-        'file' : file_list,
-        # 'content': content,
-        # 'timestamp': datetime.utcnow()
-    }
+        current_time = datetime.now() # 현재 시간
+        ext = file.filename.split('.')[-1]  # 이미지 확장자 추출
+        filename = f"{current_time.strftime('%Y%m%d%H%M%S')}.{ext}" # 이미지 이름 설정
+
+        save_to = f'static/img/upload_img/{filename}'  # 경로지정
+        file.save(save_to)  # 이미지 파일 저장
+
+        input_age = request.form['input_age']
+
+        doc = {
+            # 'user_id': user_id,
+            # 'user_nick_name': user_nick_name,
+            'img_name': filename,
+            'input_age': input_age,
+            # 'timestamp': datetime.utcnow()   
+        }
     db.posts.insert_one(doc)
-    return jsonify({'msg': '저장완료!'})
+    return jsonify({'msg': '저장완료!', 'filename': filename})
+
+
+# 파일 정보 보내기
+@app.route('/posts', methods=['POST'])
+def get_file():
+    data = json.loads(request.data)
+    filename = data.get("filename")
+    post = db.posts.find_one({'img_name': filename})
+    post["_id"] = str(post["_id"])
+    print(post)
+    return jsonify({'msg': '저장완료!', 'post': post})
+
+
+# @app.route('/posts', methods=['POST'])
+# def post_file():
+# # if user is not None:
+#     files = request.files.to_dict() # ImmutableMultiDict을 객체로 변환
+#     file_list = []
+#     for file in files.values():
+#         extension = file.filename.split('.')[-1]
+#         format = 'JPEG' if extension.lower() == 'jpg' else extension.upper()
+#         img = Image.open(file)
+#         wpercent = (614/ float(img.size[0]))
+#         h_size = int((float(img.size[1]) * float(wpercent)))
+#         img_resize = img.resize((614, h_size))
+#         buffered = BytesIO()
+#         img_resize.save(buffered, format)
+#         image_base64 = base64.b64encode(buffered.getvalue())
+#         file_list.append(image_base64)
+#     # user_id = user.get('id')
+#     # user_nick_name = user.get('nick_name')
+#     # content = request.form['content']
+#     doc = {
+#         # 'user_id': user_id,
+#         # 'user_nick_name': user_nick_name,
+#         'file' : file_list,
+#         # 'content': content,
+#         # 'timestamp': datetime.utcnow()
+#     }
+#     db.posts.insert_one(doc)
+#     return jsonify({'msg': '저장완료!'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
